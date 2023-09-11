5b203080c9a314f9cbf7b2010dfe5d4b36a72be7
THRINATH52google <49154392+THRINATH52google@users.noreply.github.com>
2023-09-12 00:10:01 +0530

    Create properties.js

---
+++ health/src/properties.js
@@ -0,0 +1,15 @@
+export const properties = {
+    base_url: 'https://na-1-dev.api.opentext.com',
+    css_url: 'https://css.na-1-dev.api.opentext.com',
+    tenant_id: '6f8fc3c5-e43c-4764-a2b6-60845cf97d0a',
+    username: '',
+    password: '',
+    client_id: 'heOhRfidX6yK7Nj96JEN8d3W1Gfy797n4W6V7',
+    client_secret: 'LJ2Y6Wjaw20GM8d5',
+    db_host: 'localhost',
+    db_port: '3306',
+    db_user: '',
+    db_password: '',
+    db_database: 'healthcare',
+    server_url: 'http://localhost:3000/'
+}
